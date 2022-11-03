a8e941b94e77a0a746c5a86ca9f9665fb61c2bbf
Evan Zhao <zevany16@gmail.com>
2022-11-02 22:50:57 -0700

    Renaming of venv file
    Lots of changes just because I wanted to practice creating a virtual environment and selecting the correct interpreter again.

--- api.py
+++ api.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/members")
+@app.route('/members')
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
-
 
 if __name__ == "__main__":
     app.run(debug=True)
