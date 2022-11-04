1085ed3317acf4221fd354f738833cc093b08bbf
Evan Zhao <zevany16@gmail.com>
2022-11-04 01:13:02 -0700

    Set own user environment variables
    Created a .env file which overrides global environment variables using python-dotenv. Doing so allows me to automatically run code in debug mode as well as use the command flask run as the FLASK_APP environment variable is now equal to the path of my python api.py file.

--- api.py
+++ api.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return{"members": ["Member1", "Member2", "Member3"]}
+@app.route("/profile")
+def myProfile():
+    response_body = {
+        "name": "Evan Zhao",
+        "about": "I'm currently in the process of learning how to connect my backend to React!"
+    }
+    return response_body
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
