29f0962ae77480a30a28ec9460be261b7230d32b
Evan Zhao <zevany16@gmail.com>
2022-11-04 21:58:08 -0700

    Unstyled Form and State
    Added an unstyled form element within a new components folder that is in src folder(figured out the hard way that react component functions MUST HAVE UPPERCASE FIRST LETTER). Removed all the get request code that displayed flask backend to react frontend using GET command. Created multiple states to contain values of user and partially through creating POST request to flask

--- api.py
+++ api.py
@@ -1,16 +1,30 @@
-from flask import Flask
-# Test import for numpy (which will probably be used)
+from flask import Flask, request, redirect
 import numpy as np
+import random
+import statistics
 
 app = Flask(__name__)
 
-@app.route("/profile")
-def myProfile():
+@app.route("/", methods = ['GET'])
+def myProfile(name, retirement_account_balance, yearly_expenses, years, stock_percentage):
     response_body = {
-        "name": "Evan Zhao",
-        "about": "I'm currently in the process of learning how to connect my backend to React!"
+        "name": name,
+        "retirement_account_balance": retirement_account_balance,
+        "yearly_expenses": yearly_expenses,
+        "years": years,
+        "stock_percentage": stock_percentage
     }
     return response_body
+
+@app.route("/retrieve", methods=['POST'])
+def retrieveData():
+    return null
+    
+    
+
+@app.route("/user/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run()
