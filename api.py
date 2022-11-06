187e8f7c755d55d8a0205a1be8df812e18b6d5d7
Evan Zhao <zevany16@gmail.com>
2022-11-05 17:34:01 -0700

    Rendered API data to React Page
    Created conditional load checking to make sure object wasn't being rendered to page before it loaded in. Modified profileData state to be an array of objects in order to make load-checking work. Passed loaded data as a prop to another React component for page display.

--- api.py
+++ api.py
@@ -5,26 +5,21 @@ import statistics
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
-def myProfile(name, retirement_account_balance, yearly_expenses, years, stock_percentage):
+@app.route("/profile", methods = ['GET'])
+def myProfile():
     response_body = {
-        "name": name,
-        "retirement_account_balance": retirement_account_balance,
-        "yearly_expenses": yearly_expenses,
-        "years": years,
-        "stock_percentage": stock_percentage
+        "name": "Evan",
+        "retirement_account_balance": 1000000,
+        "yearly_expenses": 75000,
+        "years": 25,
+        "stock_percentage": 50
     }
+    # name, retirement_account_balance, yearly_expenses, years, stock_percentage
     return response_body
 
 @app.route("/retrieve", methods=['POST'])
 def retrieveData():
     return null
-    
-    
-
-@app.route("/user/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run()
